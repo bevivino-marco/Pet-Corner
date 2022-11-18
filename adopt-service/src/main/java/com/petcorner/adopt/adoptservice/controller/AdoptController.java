bb28acca4af5d2f5b67c300051f93fe53e10e191
Marco <s206263@studenti.polito.it>
2022-11-18 20:09:50 +0100

    cross origin allowed

--- adopt-service/src/main/java/com/petcorner/adopt/adoptservice/controller/AdoptController.java
+++ adopt-service/src/main/java/com/petcorner/adopt/adoptservice/controller/AdoptController.java
@@ -3,16 +3,13 @@ package com.petcorner.adopt.adoptservice.controller;
 import com.petcorner.adopt.adoptservice.model.Animal;
 import com.petcorner.adopt.adoptservice.repository.AdoptRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1")
 public class AdoptController {
