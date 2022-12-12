aeb8401af1f32a4f773b8eb60afc47fb8e567dc8
asyamantovani <asya.mantovani@synthesis.it>
2022-12-12 11:34:27 +0100

    ok

---
+++ animal-sitting/animal-sitting/src/main/java/com/petcorner/animalsitting/animalsitting/AnimalSittingApplication.java
@@ -0,0 +1,13 @@
+package com.petcorner.animalsitting.animalsitting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalSittingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalSittingApplication.class, args);
+	}
+
+}
---
+++ animal-sitting/animal-sitting/src/test/java/com/petcorner/animalsitting/animalsitting/AnimalSittingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petcorner.animalsitting.animalsitting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalSittingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
