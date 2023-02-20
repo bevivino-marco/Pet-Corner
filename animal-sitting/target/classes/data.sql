46bc0ab4e2ec2b200701d2366d1da95a940a660d
Marco <s206263@studenti.polito.it>
2023-02-20 21:42:30 +0100

    Tutto funzionante su k8s

---
+++ animal-sitting/target/classes/data.sql
@@ -0,0 +1,28 @@
+insert into sitter
+(id, name,surname, age, locality, personal_description, animals_allowed, size_allowed)
+values(1, 'Asya','Mantovani',23,'Torino','Fin da piccola ho sempre avuto una passione per gli animali, in particolare i cani. Possiedo da 11 anni un barboncino e da qualche mese un dogo argentino','dog_cat');
+
+insert into sitter
+(id, name,surname, age, locality, personal_description, animals_allowed, size_allowed)
+values(2, 'Mario','Rossi',40,'Messina','esperienza di 30 anni in mansioni di animal sitting','dog_cat_reptile_birdsOfPrey_birds_rodents');
+
+insert into sitter
+(id, name,surname, age, locality, personal_description, animals_allowed, size_allowed)
+values(3, 'Giovanni','Rossi',32,'Cosenza','esperto di rettili','reptile');
+
+insert into sitter
+(id, name,surname, age, locality, personal_description, animals_allowed, size_allowed)
+values(4, 'Marianna','Rossi',27,'Venezia','gattara dalla nascita','cat');
+
+insert into sitter
+(id, name,surname, age, locality, personal_description, animals_allowed, size_allowed)
+values(5, 'Alessandro','Muraro',24,'Ovada','fin da piccolo avevo una passione per i pesci che coltivo ancora adesso','fish');
+
+insert into sitter
+(id, name,surname, age, locality, personal_description, animals_allowed, size_allowed)
+values(6, 'Marco','Bevivino',27,'Bolzano','esperto di cani','dog');
+
+insert into sitter
+(id, name,surname, age, locality, personal_description, animals_allowed, size_allowed)
+values(7, 'Nicole','Morando',25,'Roma','esperto di rapaci e uccelli','birdsOfPrey_birds');
+
